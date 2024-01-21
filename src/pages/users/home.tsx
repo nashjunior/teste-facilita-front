761c24dd5dbad71943310790f32b9f10fe2bf9fb
Nash Junior <nashjuniorjj@gmail.com>
2024-01-21 18:28:43 -0300

    feat: add home and new pages

--- src/pages/users/home.tsx
+++ src/pages/users/home.tsx
@@ -1,45 +1,158 @@
-import { Heading } from '@chakra-ui/react';
+import {
+  AlertDialog,
+  AlertDialogOverlay,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogCloseButton,
+  AlertDialogBody,
+  AlertDialogFooter,
+  useDisclosure,
+  Button,
+  Heading,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useToast,
+} from '@chakra-ui/react';
 import React from 'react';
 import DataTable from '../../components/datatable';
 import { t } from 'i18next';
 import {
-  type IColumn,
   type IRemoteDatatableOptions,
+  type IColumn,
   type IDatatableOption,
 } from '../../components/datatable/interfaces';
-import { type ICustomer } from '../../definitions';
-import { apiBase } from '../../config/api-base';
 import { FaPencil } from 'react-icons/fa6';
-import { FaTrash } from 'react-icons/fa';
+import { FaRoute, FaSave, FaTimes, FaTrash } from 'react-icons/fa';
+import { type ICreateUser, type IUser } from '../../definitions';
+import FormClient from './form-client';
+import { FormProvider, type SubmitHandler, useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { createUserSchema } from '../../validations';
+import { createCoordinateSchema } from '../../validations/create-coordinate';
+import { type ICreateCoordinate } from '../../definitions/coordinate';
+import { FormCoordinate } from './form-coordinate';
+import { apiBase } from '../../config/api-base';
+import { TextField } from '../../components/form/textfield';
+import { useDispatch } from 'react-redux';
+import { deleteClient, updateClient } from '../../actions';
+
+type IFormUpdateUser = ICreateUser & ICreateCoordinate;
 
 const Customers: React.FC = () => {
-  const columns: Array<IColumn<ICustomer>> = [
+  const dispatch = useDispatch();
+  const toast = useToast();
+  const modalEditClient = useDisclosure();
+  const modalGenerateRoutes = useDisclosure();
+  const [queryValue, setQueryValue] = React.useState<string>();
+  const dialogDeleteUser = useDisclosure();
+  const refUser = React.useRef<IUser | null>(null);
+  const cancelRef = React.useRef<HTMLDialogElement | null>(null);
+  const refClientsRoutes = React.useRef<IUser[]>();
+
+  const methods = useForm<IFormUpdateUser>({
+    resolver: yupResolver<any>(createUserSchema.concat(createCoordinateSchema)),
+    defaultValues: { phoneNumber: '', email: '', name: '', coordinates: [] },
+  });
+
+  const columns: Array<IColumn<IUser>> = [
     { field: 'name', text: t('name') },
     { field: 'email', text: 'Email' },
-    { field: 'cpf', text: t('cpf') },
+    { field: 'phoneNumber', text: t('phoneNumber') },
   ];
 
-  const serverData: IRemoteDatatableOptions<ICustomer>['serverData'] = {
-    url: `${apiBase.defaults.baseURL}/customers`,
+  const selectedClientId = React.useRef<string | null>(null);
+
+  const handleGetRoutes = async (): Promise<void> => {
+    const { data } = await apiBase.get<IUser[]>('routes/generate');
+
+    refClientsRoutes.current = data;
+  };
+
+  const handleCloseModalGenerateRoutes = (): void => {
+    refClientsRoutes.current = [];
+    modalGenerateRoutes.onClose();
+  };
+
+  const handleUpdateClient: SubmitHandler<IFormUpdateUser> = async (
+    data
+  ): Promise<void> => {
+    try {
+      await dispatch(
+        updateClient({ id: selectedClientId.current as string, ...data }) as any
+      );
+      methods.reset({ coordinates: [] });
+      toast({
+        title: t('toastSucessCreateClientTitle'),
+        description: t('toastSucessCreateClientDescription'),
+        status: 'success',
+        duration: 5000,
+        isClosable: true,
+        position: 'top-right',
+      });
+    } catch (error) {
+      toast({
+        title: 'Error!',
+        description: 'Could not update user!',
+        status: 'error',
+        duration: 5000,
+        isClosable: true,
+        position: 'top-right',
+      });
+    }
+  };
+
+  const serverData: IRemoteDatatableOptions<IUser>['serverData'] = {
+    url: `${apiBase.defaults.baseURL}/clients`,
     serverPagination: true,
   };
 
-  const options: IDatatableOption<ICustomer> = {
+  const options: IDatatableOption<IUser> = {
+    filters: [
+      {
+        component: (
+          <TextField
+            Label="pesquisar"
+            onChange={(e) => {
+              setQueryValue(e.target.value === '' ? undefined : e.target.value);
+            }}
+            value={queryValue}
+          />
+        ),
+        field: 'query',
+        value: queryValue,
+      },
+      {
+        component: <></>,
+        field: 'query_fields',
+        value: 'c.name,c.email,c.phone_number',
+      },
+    ],
     actions: {
-      headerText: t('name'),
+      headerText: t('actions'),
       items: [
         {
           icon: <FaPencil />,
           tooltip: t('edit'),
           getRow: (row) => {
-            console.log(row);
+            selectedClientId.current = row.id;
+            methods.setValue('email', row.email);
+            methods.setValue('name', row.name);
+            methods.setValue('phoneNumber', row.phoneNumber);
+            modalEditClient.onOpen();
           },
         },
         {
           icon: <FaTrash />,
           tooltip: t('delete'),
           getRow: (row) => {
-            console.log(row);
+            refUser.current = row;
+
+            dialogDeleteUser.onOpen();
           },
         },
       ],
@@ -48,7 +161,19 @@ const Customers: React.FC = () => {
 
   return (
     <React.Fragment>
-      <Heading mt={8}>{t('customerList')}</Heading>
+      <Heading mt={8} mb={4}>
+        {t('userList')}
+      </Heading>
+      <Button
+        leftIcon={<FaRoute />}
+        colorScheme="telegram"
+        onClick={() => {
+          void handleGetRoutes();
+          modalGenerateRoutes.onOpen();
+        }}
+      >
+        Generate routes
+      </Button>
       <DataTable
         columns={columns}
         options={options}
@@ -58,6 +183,110 @@ const Customers: React.FC = () => {
         mt={4}
         mb={12}
       />
+
+      <Modal
+        isOpen={modalGenerateRoutes.isOpen}
+        onClose={handleCloseModalGenerateRoutes}
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Route order</ModalHeader>
+          <ModalCloseButton />
+
+          <ModalBody>asdfasf</ModalBody>
+
+          <ModalFooter>
+            <Button
+              colorScheme="blue"
+              mr={3}
+              onClick={handleCloseModalGenerateRoutes}
+              leftIcon={<FaTimes />}
+            >
+              Close
+            </Button>
+            <Button colorScheme="green" leftIcon={<FaSave />} type="submit">
+              {t('save')}
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+
+      <Modal isOpen={modalEditClient.isOpen} onClose={modalEditClient.onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Edit client</ModalHeader>
+          <ModalCloseButton />
+          <FormProvider {...methods}>
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+                void methods.handleSubmit(handleUpdateClient)();
+              }}
+            >
+              <ModalBody>
+                <FormClient />
+                <FormCoordinate />
+              </ModalBody>
+
+              <ModalFooter>
+                <Button
+                  colorScheme="blue"
+                  mr={3}
+                  onClick={modalEditClient.onClose}
+                  leftIcon={<FaTimes />}
+                >
+                  Close
+                </Button>
+                <Button colorScheme="green" leftIcon={<FaSave />} type="submit">
+                  {t('save')}
+                </Button>
+              </ModalFooter>
+            </form>
+          </FormProvider>
+        </ModalContent>
+      </Modal>
+
+      <AlertDialog
+        motionPreset="slideInBottom"
+        leastDestructiveRef={cancelRef}
+        onClose={dialogDeleteUser.onClose}
+        isOpen={dialogDeleteUser.isOpen}
+        isCentered
+        size="md"
+      >
+        <AlertDialogOverlay />
+
+        <AlertDialogContent>
+          <AlertDialogHeader fontSize="lg" fontWeight="bold">
+            {t('dialogHeaderDeleteClient')}
+          </AlertDialogHeader>
+          <AlertDialogCloseButton />
+          <AlertDialogBody>{t('dialogBodyDeleteClient')}</AlertDialogBody>
+          <AlertDialogFooter>
+            <Button
+              colorScheme="blue"
+              ml={3}
+              onClick={dialogDeleteUser.onClose}
+              leftIcon={<FaTimes />}
+            >
+              {t('close')}
+            </Button>
+            <Button
+              colorScheme="red"
+              leftIcon={<FaTrash />}
+              ml={3}
+              // eslint-disable-next-line @typescript-eslint/no-misused-promises
+              onClick={async () => {
+                await dispatch(
+                  deleteClient(refUser.current?.id as string) as any
+                );
+              }}
+            >
+              {t('delete')}
+            </Button>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
     </React.Fragment>
   );
 };
--- src/pages/users/new.tsx
+++ src/pages/users/new.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { type SubmitHandler, useForm, FormProvider } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { createUserSchema } from '../../validations';
 import {
-  Box,
   Button,
   type StackProps,
   VStack,
   useBreakpointValue,
   useToast,
+  Center,
 } from '@chakra-ui/react';
 import { FaSave } from 'react-icons/fa';
 import { t } from 'i18next';
 import { type ICreateUser } from '../../definitions';
+import { selectUserRequestStatus } from '../../selectors';
+import { createClient } from '../../actions';
 
 const FormClient = React.lazy(async () => await import('./form-client'));
 
@@ -24,15 +26,10 @@ const NewClient: React.FC = () => {
   const navigate = useNavigate();
   const methods = useForm<ICreateUser>({
     resolver: yupResolver<any>(createUserSchema),
-    defaultValues: {
-      // name: '',
-      // jobTitle: '',
-      // cpf: '',
-      // email: '',
-      // password: '',
-      // confirmPassword: '',
-    },
+    defaultValues: { phoneNumber: '', email: '', name: '' },
   });
+
+  const isLoading = useSelector(selectUserRequestStatus);
 
   const variant = useBreakpointValue<StackProps>(
     { md: { w: '75%', mt: 8 }, base: { w: '100%' } },
@@ -42,16 +39,29 @@ const NewClient: React.FC = () => {
   const handleCreateClient: SubmitHandler<ICreateUser> = async (
     data
   ): Promise<void> => {
-    // void handleCreateData(data);
-    console.log(data);
-    toast({
-      title: 'Sucesso!',
-      description: 'Arquivo baixado com sucesso!',
-      status: 'success',
-      duration: 5000,
-      isClosable: true,
-      position: 'top-right',
-    });
+    try {
+      await dispatch(createClient(data) as any);
+      console.log(data);
+      toast({
+        title: t('toastSucessCreateClientTitle'),
+        description: t('toastSucessCreateClientDescription'),
+        status: 'success',
+        duration: 5000,
+        isClosable: true,
+        position: 'top-right',
+      });
+
+      navigate('/users');
+    } catch (error) {
+      toast({
+        title: 'Error!',
+        description: 'Could not create user!',
+        status: 'error',
+        duration: 5000,
+        isClosable: true,
+        position: 'top-right',
+      });
+    }
   };
 
   return (
@@ -59,8 +69,7 @@ const NewClient: React.FC = () => {
       <VStack justifyContent="center" alignItems="center">
         <VStack {...variant} bgColor="white">
           <FormProvider {...methods}>
-            <Box
-              as="form"
+            <form
               onSubmit={(e) => {
                 e.preventDefault();
                 void methods.handleSubmit(handleCreateClient)();
@@ -68,15 +77,18 @@ const NewClient: React.FC = () => {
             >
               <FormClient />
 
-              <Button
-                my={4}
-                rightIcon={<FaSave />}
-                colorScheme="green"
-                type="submit"
-              >
-                {t('save')}
-              </Button>
-            </Box>
+              <Center>
+                <Button
+                  my={4}
+                  rightIcon={<FaSave />}
+                  colorScheme="green"
+                  type="submit"
+                  isLoading={isLoading}
+                >
+                  {t('save')}
+                </Button>
+              </Center>
+            </form>
           </FormProvider>
         </VStack>
       </VStack>
